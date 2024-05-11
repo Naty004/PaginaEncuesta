0e8d52b67d1f999787ede0c633d3515eddc2b98c
Natt004 <marlesnataly@gmail.com>
2024-05-11 02:18:00 -0500

    Proyecto con pagina principal

---
+++ resources/views/paginaPrincipal.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Principal</title>
+    <link rel="stylesheet" href="{{ asset('css/estiloPaginaPrincipal.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+</div>
+</div>
+</head>
+<body>
+
+    <header>
+        <div class="logo">
+            <h1>Encuesta</h1>
+            </div>
+            <section class="botones">
+                <a href="#" class="boton">Registrarse</a> 
+                <a href="#" class="boton">Iniciar sesión</a>
+            </section>
+    </header>
+
+    
+    <section class="imagen-con-degradado">
+        <div class="contenido">
+            <h2>Únete a nosotros y sé parte del cambio que el mundo necesita. Juntos, podemos construir un futuro laboral más justo y humano. ¡Tu voz y tu acción marcan la diferencia!</h2>
+            <a href="#" class="boton">Unirse</a> 
+        </div>
+    </section>
+
+
+    <section class="tematica1">
+          
+    <h2 class="center"> ¡TU CONTRIBUCIÓN MARCA LA DIFERENCIA!</h2>
+
+        <p>
+            Enfrentar el trabajo forzoso y la esclavitud moderna no es solo un imperativo moral, es una responsabilidad compartida que requiere el compromiso de todos.
+            En nuestra plataforma, invitamos a empresas y trabajadores a unirse a esta causa crucial. Juntos, podemos transformar la realidad laboral, promoviendo
+            entornos seguros, justos y libres de explotación. Al evaluar y compartir nuestro progreso en la implementación de medidas contra el trabajo forzoso, 
+            estamos construyendo un mundo donde cada individuo pueda prosperar con dignidad y respeto. </p>
+        
+
+  </section> 
+
+
+            <h2>LA ESCLAVITUD  MODERNA</h2>
+    <section class="tematica2">
+        <p>La esclavitud moderna es la versión actualizada de la esclavitud que conocemos de la historia, pero en lugar de cadenas y campos de trabajo, se manifiesta de diferentes maneras en la actualidad. 
+            Implica que las personas son tratadas como propiedad y son obligadas a trabajar bajo condiciones muy malas, sin tener la opción de dejar el trabajo o ganar dinero justo por su labor.
+             Puede incluir situaciones como ser obligado a trabajar sin pago, ser víctima de tráfico humano, estar atrapado en deudas que nunca se pueden pagar o ser explotado sexualmente. Es básicamente aprovecharse de las personas de manera injusta y cruel.
+        </p>     
+
+        <img src="{{ asset('images/Esclavitud moderna.webp') }}" width="800" height="400">
+
+</section>
+<section class="fondoTexto">
+<h2>EL TRABAJO FORZOSO</h2>
+</section>
+
+<section class="color1">
+    <section class="tematica3">
+        <p>El trabajo forzoso se refiere a cualquier forma de trabajo o servicio realizado por una persona bajo la amenaza de penalidades o coerción, ya sea física o psicológica.
+             Esto puede incluir la servidumbre por deudas, el trabajo infantil, la trata de personas y otras prácticas similares que privan a los individuos de su libertad y dignidad.
+              Es una violación grave de los derechos humanos y está prohibido por el derecho internacional.</p>     
+            </section>
+</section>
+        <section class="color1">
+        <div class="seccion-imagenes">
+    <img src="{{ asset('images/Niños.jpg') }}" width="400" height="300">
+    <img src="{{ asset('images/Trabajo1.jpg') }}"  width="400" height="300">
+    <img src="{{ asset('images/Niños3.jpg') }}"  width="400" height="300">
+    <img src="{{ asset('images/Niños2.jpg') }}"  width="400" height="300">
+    <img src="{{ asset('images/Trabajo2.jpg') }}"  width="400" height="300">
+    <img src="{{ asset('images/Trabajo3.jpg') }}"  width="400" height="300">
+</div>
+</section>
+
+<h2>PAÍSES CON MAYOR CANTIDAD DE TRABAJO FORZOSO</h2>
+    <section class="color2">
+        </section>
+        <div class="cuadrados">
+    <div class="cuadrado">Estados unidos 403.000 victimas
+    </div>
+    <div class="cuadrado">Brasil 369.000 victimas
+    </div>
+    <div class="cuadrado">Mexico
+        341.000 victimas</div>
+</section>
+
+</div> 
+<section class=final>
+    <h2> Registrate ahora </h2>
+    <section>
+    <div> <span class="negrita">Empresas: </span> Haz que tu empresa avance, siguiendo las recomendaciones adecuadas para erradicar el trabajo forzoso. 
+</div> <span class="negrita">Trabajadores:</span> Registrate y afirma tu situación.
+<div>   
+ <section class="center">   
+<a href="#" class="boton">Registrarse</a> 
+</section>
+</section>
+</section>
+
